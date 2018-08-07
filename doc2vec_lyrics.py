999d5a948d1b21d66a7e7333ead1ac7a1045f8ba
franciscoGPS <fco.coma@gmail.com>
2018-08-07 02:58:56 +0100

    audio and lyric models tensors concatenated

--- doc2vec_lyrics.py
+++ doc2vec_lyrics.py
@@ -5,51 +5,54 @@ from sentences import Sentences
 from gensim.models.doc2vec import Doc2Vec
 import nltk
 
+class doc2vec_model():
+  
+  def __init__(self, basedir, lyrics_filename="none", model_name="lyrics.doc2vec"):
+    self.basedir = basedir
+    self.model_name = model_name
+    self.lyrics_filename = lyrics_filename
 
-#file_location="/media/frisco/FAT/cross-modal/synced_audios/"
-file_location="/home/frisco/Documents/Nube/Projects/python/cross-modal/spotify/"
-file_name="Synced_SongsMoodsFile.csv"
-files = [os.path.join(file_location,fn) for fn in os.listdir(file_location) if fn.endswith('Synced_SongsMoodsFile.csv')]
+  def load_model(self):
+    ipdb.set_trace
+    files = [os.path.join(self.basedir,fn) for fn in os.listdir(self.basedir) if fn.endswith(self.model_name)]
+    if len(files) > 0:
 
-for file in files:
-	songs_lyrics_ids_df = pd.read_csv(file, delimiter=',', encoding="utf-8")
-	
-	
-	songs_lyrics_ids_df.head()
+      return Doc2Vec.load(self.basedir+self.model_name)
+    else:
+      files = [os.path.join(self.basedir,fn) for fn in os.listdir(self.basedir) if fn.endswith(self.lyrics_filename)]
+      for file in files:
+      	songs_lyrics_ids_df = pd.read_csv(file, delimiter=',', encoding="utf-8")
+      	
+      	
+      	songs_lyrics_ids_df.head()
 
-	group = ['lyric','mood' ]
-	lyrics_by_song = songs_lyrics_ids_df.sort_values(group)\
-       .groupby(group).lyric\
-       .apply(' '.join)\
-       #.reset_index(name='lyric')
-	lyrics_by_song.head(1)
-	file_name = file
+      	group = ['lyric','mood' ]
+      	lyrics_by_song = songs_lyrics_ids_df.sort_values(group)\
+             .groupby(group).lyric\
+             .apply(' '.join)\
+             #.reset_index(name='lyric')
+      	lyrics_by_song.head(1)
+      	file_name = file
 
-filename=file_name
-nltk.download('wordnet')
-sentences = Sentences(filename=filename, column="lyric")
-df_train = pd.read_csv(filename,
-        index_col=[0],
-        usecols=[0,3],
-        header=0,
-        names=["mood", "lyric"])
-ipdb.set_trace()
-model = Doc2Vec(alpha=0.025,min_alpha=0.025,workers=15, min_count=2,window=10,size=300,iter=20,sample=0.001,negative=5)
+      filename=file_name
+      nltk.download('wordnet')
+      sentences = Sentences(filename=filename, column="lyric")
+      df_train = pd.read_csv(filename,
+              index_col=[0],
+              usecols=[0,3],
+              header=0,
+              names=["mood", "lyric"])
 
+      model = Doc2Vec(alpha=0.025,min_alpha=0.025,workers=15, min_count=2,window=10,size=200,iter=20,sample=0.001,negative=5)
+      model.build_vocab(sentences)
+      epochs = 10
+      for epoch in range(epochs):
+          model.train(sentences, total_examples=model.corpus_count, epochs=model.iter)
+          model.alpha -= 0.002  # decrease the learning rate
+          model.min_alpha = model.alpha  # fix the learning rate, no decay
+      model.save("lyrics.doc2vec")
+      
+      return model
 
-
-
-model.build_vocab(sentences)
-epochs = 10
-
-for epoch in range(epochs):
-    model.train(sentences, total_examples=model.corpus_count, epochs=model.iter)
-    model.alpha -= 0.002  # decrease the learning rate
-    model.min_alpha = model.alpha  # fix the learning rate, no decay
-
-
-    model.save('lyrics.doc2vec')
-
-	
-
-#model = Doc2Vec.load('lyrics.doc2vec')
+      #model.infer_vector(["system", "response", "Error"])
+  
--- keras_build.py
+++ keras_build.py
@@ -6,6 +6,7 @@ import numpy as np
 import ipdb
 import pandas as pd
 import scipy.io.wavfile as wav
+from scipy.stats import rankdata
 import os
 import keras
 import tensorflow as tf
@@ -13,14 +14,7 @@ from keras import optimizers
 from keras import callbacks
 from keras.backend.tensorflow_backend import set_session
 from keras.models import Sequential
-from keras.layers import Activation
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.layers import Embedding
-from keras.layers import TimeDistributed
-from keras.layers import Bidirectional
-from keras.layers import Flatten
-from keras.layers import Dropout
+from keras.layers import *
 from keras.utils import to_categorical
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn import preprocessing
@@ -29,6 +23,8 @@ from sklearn import pipeline
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import label_ranking_average_precision_score
+from sklearn.metrics.scorer import make_scorer
 from sklearn.grid_search import GridSearchCV
 from sklearn.externals import joblib
 from numpy import argmax
@@ -36,6 +32,9 @@ from python_speech_features import mfcc
 from gammatone import gtgram
 from matplotlib import pyplot as plt
 import skfuzzy as fuzz
+import corenlp
+import doc2vec_lyrics as d2v
+
 
 tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
 filesystem_path = (os.path.abspath(os.path.join(os.path.dirname("__file__"), '../')) + '/spotify/')
@@ -49,21 +48,38 @@ MOODS = ['aggressive', 'bittersweet', 'calm', 'depressing', 'dreamy', 'fun', 'ga
 
 
 
-def start_process(inputfile):
+def start_process(inputfile, samples, gtg):
    results = pd.DataFrame()
    #tf.enable_eager_execution()
    devide_mfcc = False
-   (labels1,  x_mfcc1) =  extract_data(inputfile, devide_mfcc)
    test_size = 0.3
-   hidden_size = 50
+   if gtg:
+
+      (labels1,  x_mfcc1, labels_gtg, x_gtg, bow_infered, lyrics_infered) =  extract_data(inputfile, devide_mfcc, gtg, samples)
+      #gtg_X_train, gtg_X_test, gtg_y_train, gtg_y_test = split_data(labels_gtg, x_gtg, test_size )
+      gtg_X_train, gtg_X_test, gtg_y_train, gtg_y_test, bow_X_train, bow_X_test, lyr_X_train, lyr_X_test = train_test_split(x_gtg, labels_gtg, bow_infered, lyrics_infered,  test_size=test_size )
+   else:
+
+
+      (labels1,  x_mfcc1, bow_infered, lyrics_infered) =  extract_data(inputfile, devide_mfcc, gtg, samples)
+
+
    mfcc1_X_train, mfcc1_X_test, mfcc1_y_train, mfcc1_y_test = split_data(labels1, x_mfcc1, test_size)
-   num_classes = 19
 
-   print(mfcc1_X_train.shape)
+   #evaluate_dataset(mfcc1_X_train, mfcc1_y_train,  mfcc1_X_test , mfcc1_y_test)
 
+   #evaluate_dataset( gtg_X_train, gtg_y_train, gtg_X_test , gtg_y_test)
+
+   audio_model = audio_dnn(gtg_X_train, gtg_y_train)
+
+   lyrics_model = audio_dnn(bow_X_train, gtg_y_train)
+
+   mergedOut = Concatenate()([audio_model.output,lyrics_model.output])
+
+   ipdb.set_trace()
    """
    mfcc2_X_train, mfcc2_X_test, mfcc2_y_train, mfcc2_y_test = split_data(labels2, x_mfcc2, test_size )
-   gtg_X_train, gtg_X_test, gtg_y_train, gtg_y_test = split_data(labels_gtg, x_gtg, test_size )
+   
 
    gtg_model_1 = Sequential()
    results['gtg'] = train_double_LSTM(gtg_X_train, gtg_y_train, gtg_model_1, hidden_size)
@@ -87,10 +103,10 @@ def start_process(inputfile):
    mfcc1_X_trainT = mfcc1_X_train.reshape(mfcc1_X_train.shape[0],mfcc1_X_train.shape[2]).T
    mfcc2_X_trainT = mfcc2_X_train.reshape(mfcc2_X_train.shape[0],mfcc2_X_train.shape[2]).T
    mfcc2_X_testT = mfcc2_X_test.reshape(mfcc2_X_test.shape[0], mfcc2_X_test.shape[2]).T
-   cntr, uc, u0c, dc, jm, pc, fpc = fuzz.cluster.cmeans(mfcc2_X_trainT, 19, 2, error=0.005, maxiter=1000, init=None)
-   u0p, up, dp, jm, pp, fpc fuzz.cluster.cmeans_predict(mfcc2_X_testT, cntr, 4, error=0.0000005, maxiter=1000)
-   ipdb.set_trace()
+
+
    
+ 
 
    gtg_model_2b = Sequential()
    results['gtg_b'] = train_LSTM_model(gtg_X_train, gtg_y_train, gtg_model_2b, hidden_size)
@@ -116,31 +132,14 @@ def start_process(inputfile):
    """
 
    
-   #scikitmodel = create_LSTM_model(mfcc1_X_train, mfcc2_y_train, hidden_size)
-   model_classifier = KerasClassifier(build_fn=create_BLSTM_model, epochs=150, batch_size=10, verbose=1)
-   validator = GridSearchCV(model_classifier,
-                         param_grid={# epochs is avail for tuning even when not
-                                     # an argument to model building function
-                                     #'input_size': [int(mfcc1_X_train.shape[2])],
-                                     'epochs': [10, 50, 100],
-                                     #'hidden_size':[25,50,100],
-                                     },
-                         scoring='neg_log_loss',
-                         n_jobs=1)
-   validator.fit(mfcc1_X_train, mfcc1_y_train)
+   #scikitmodel = create_LSTM_model(mfcc1_X_train, mfcc2_y_train, hidden_size) 
+   #, fit_params = dict(callbacks=[tbCallBack, early_stopping])
+   
 
-   print('The parameters of the best model are: ')
-   print(validator.best_params_)
 
-   # validator.best_estimator_ returns sklearn-wrapped version of best model.
-   # validator.best_estimator_.model returns the (unwrapped) keras model
-   best_model = validator.best_estimator_.model
-   best_model.summary()
-   metric_names = best_model.metrics_names
-   metric_values = best_model.evaluate(mfcc1_X_test, mfcc1_y_test)
-   for metric, value in zip(metric_names, metric_values):
-       print(metric, ': ', value)
-   ipdb.set_trace()
+
+
+
 
    """
    # evaluate using 10-fold cross validation
@@ -171,8 +170,60 @@ def start_process(inputfile):
    joblib.dump(pipe, os.path.join(directory, 'pipeline.pkl'))
    models.save_model(model_step.model, os.path.join(directory, 'model.h5'))
    """
-   ipdb.set_trace
 
+def ranking_average_prescision(y, y_pred_probs):
+   cntr, uc, u0c, dc, jm, pcd, fpc = fuzz.cluster.cmeans(X.T, 19, 2, error=0.005, maxiter=1000, init=None)
+   u0p, up, dp, jm, pp, fpc =  fuzz.cluster.cmeans_predict(up, cntr, 4, error=0.0005, maxiter=1000)
+   ranking_score = label_ranking_average_precision_score(y, u0p.T)
+
+def ranking_precision(y, y_pred_probs):
+   presicion = 0
+   for test in range(len(y)):
+      presicion += 1 - rankdata(y_pred_probs[test])[np.argmax(y[test])-1]/len(y_pred_probs[test])
+
+   score = presicion/len(y_pred_probs)
+
+   return score
+
+def evaluate_dataset(X_train, y_train, X_test, y_test):
+   early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=1, verbose=2, mode='auto')
+   
+   model_classifier = KerasClassifier(build_fn=create_BLSTM_model, epochs=50, batch_size=10,
+    verbose=1, validation_split=0.3)
+
+
+   my_scorer = make_scorer(ranking_precision, greater_is_better=True, needs_proba=True)
+
+   size = X_train.shape[2]
+   validator = GridSearchCV(model_classifier,
+                         param_grid={# epochs is avail for tuning even when not
+                                     # an argument to model building function
+                                     #'input_size': [int(mfcc1_X_train.shape[2])],
+                                     'epochs': [ 100, 120],
+                                     'hidden_size':[50 , 75, size, size*2],
+                                     'dropout': [0.5,0.6],
+                                     'size': [size]
+                                     },
+                         scoring=my_scorer,
+                         n_jobs=1)
+   
+   validator.fit(X_train, y_train)
+
+   print('The parameters of the best model are: ')
+   print(validator.best_params_)
+
+   # validator.best_estimator_ returns sklearn-wrapped version of best model.
+   # validator.best_estimator_.model returns the (unwrapped) keras model
+   best_model = validator.best_estimator_.model
+   best_model.summary()
+
+   metric_names = best_model.metrics_names
+   metric_values = best_model.evaluate(X_test, y_test)
+   
+
+   for metric, value in zip(metric_names, metric_values):
+       print(metric, ': ', value)
+   
    
 def get_lyrics_dict():
 
@@ -187,64 +238,78 @@ def get_lyrics(video_id):
    bow = query_result['bow'].values[0]
    lyric = query_result['lyric'].values[0]
 
-def extract_data(audioset_path, devide_mfcc):
+def extract_data(audioset_path, devide_mfcc=True, calculate_gtg=True, samples=-1):
+
+   model_directory = os.path.dirname(os.path.realpath(__file__))+ "/apnews_dbow/"
+
    saved_files = [os.path.join(audioset_path,fn) for fn in os.listdir(audioset_path) if fn.endswith('.wav')]
    labels_1 = []
    labels_2 = []
    labels_gtg = []
+   lyrics_infered = []
+   bow_infered = []
+   tokens = []
    x_mfcc_1 = []
    x_mfcc_2 = []
    x_gtg = []
    counter = 0
    errors = {"error":[], "counter":[]}
-   twin = 0.5
-   thop = .250
-   channels = 4
+   twin = 1
+   thop = .7
+   channels = 1
    fmin = 20
 
-
-   #lyrycs_df = get_lyrics_dict()
+   doc2vec = d2v.doc2vec_model(model_directory, model_name="doc2vec.bin", lyrics_filename="integral_synced_SongsMoodsFile.csv")
    
+   doc2vec_model = doc2vec.load_model()
 
 
-   for audiopath in saved_files[:10]:
-      
+   lyrycs_df = get_lyrics_dict()
+   for audiopath in saved_files[:samples]:
       try:
-
         # if audiop5000.split("/")[-1].split("_")[0] in ['aggressive', 'happy', 'sad', 'sweet']:
-         
          (rate,sig) = wav.read(audiopath)
          duration = len(sig) / rate
-
          if duration >= 30:
             #signal, rate, freq = 0.5, numcep=1, winlen = 1, winstep = 0.5 #
-            mfcc_1 = get_mfcc(sig, rate, 1, 4, 2, 1)
+            mfcc_1 = get_mfcc(sig, rate, 1, channels, twin, thop)
             print("mfcc_1.shape:", mfcc_1.shape)
-
-            """
-            gtg = get_gtg(rate, sig, twin, thop, channels, fmin)
-            print("gtg.shape: ", gtg.shape)
-            gtg_to_mfcc = get_gtg(rate, sig, twin, thop, 4, fmin)
-            mfcc_2 = get_mfcc(gtg_to_mfcc, rate, 0.3, 13, 1, .5)
-            print("mfcc_2.shape", mfcc_2.shape)
-            labels_gtg.append(int(MOODS.index(mood)))
-            x_gtg.append(np.array(gtg.reshape(1, gtg.size)).astype(np.float32))
-
-            """
-
-
+            
             audio_name = audiopath.split("/")[-1]
             mood = audio_name.split("_")[0]
             video_id = audio_name[-15:-4]
+            query_result = lyrycs_df.query('youtube_video_id ==  @video_id')
+            lyc = query_result['lyric'].values[0]
+            bow = query_result['bow'].values[0]
+            # Tokenize, its not working. We already have cleaned lyrics.
+            # client =  corenlp.CoreNLPClient(annotators="tokenize ssplit sentiment".split())
+            #tokenzed_lyc = client.annotate(lyc)
+            #tokenzed_bow = client.annotate(bow)
+            bow_infered.append(doc2vec_model.infer_vector(bow))
+            lyrics_infered.append(doc2vec_model.infer_vector(lyc))
+
+
+            if calculate_gtg:
+               gtg = get_gtg(rate, sig, twin, thop, channels, fmin)
+               print("gtg.shape: ", gtg.shape)
+               labels_gtg.append(int(MOODS.index(mood)))
+               x_gtg.append(np.array(gtg.reshape(1, gtg.size)).astype(np.float32))
+
+            """
+            gtg_to_mfcc = get_gtg(rate, sig, twin, thop, 4, fmin)
+            mfcc_2 = get_mfcc(gtg_to_mfcc, rate, 0.3, 13, 1, .5)
+            print("mfcc_2.shape", mfcc_2.shape)
             
             
+
+            """
+
             if devide_mfcc:
                for seq in range(len(mfcc_1)):
-                  
                   x_mfcc_1.append(np.array(mfcc_1[seq].reshape(1, mfcc_1[seq].size)).astype(np.float32))
+                  labels_1.append(int(MOODS.index(mood)))
             else:
                labels_1.append(int(MOODS.index(mood)))
-   
                x_mfcc_1.append(np.array(mfcc_1.reshape(1, mfcc_1.size)).astype(np.float32))
                #x_gtg.append(np.array(gtg.reshape(1, gtg.size)).astype(np.float32))
                #x_gtg.append(gtg.reshape(gtg.size,1))
@@ -258,18 +323,20 @@ def extract_data(audioset_path, devide_mfcc):
       
    labels_np1 = np.array(labels_1).astype(dtype=np.uint8)
    #labels_np2 = np.array(labels_2).astype(dtype=np.uint8)
-   #labels_gtg = np.array(labels_gtg).astype(dtype=np.uint8)
-
-   
+   bow_infered = np.array(bow_infered).astype(np.float32)
+   lyrics_infered = np.array(lyrics_infered).astype(np.float32)
    x_mfcc_np1 = np.array(x_mfcc_1).astype(np.float32)
    #x_mfcc_np2 = np.array(x_mfcc_2).astype(np.float32)
-   #x_gtg_np = np.array(x_gtg).astype(np.float32)
-
    labels_onehot1 = (np.arange(19) == labels_np1[:, None]).astype(np.float32)
+   
+   if calculate_gtg:
+      labels_gtg = np.array(labels_gtg).astype(dtype=np.uint8)
+      x_gtg_np = np.array(x_gtg).astype(np.float32)
+      labels_onehotgtg = (np.arange(19) == labels_gtg[:, None]).astype(np.float32)
+      return labels_onehot1, x_mfcc_np1, labels_onehotgtg, x_gtg_np, bow_infered, lyrics_infered
    #labels_onehot2 = (np.arange(19) == labels_np2[:, None]).astype(np.float32)
-   #labels_onehotgtg = (np.arange(19) == labels_gtg[:, None]).astype(np.float32)
-
-   return labels_onehot1, x_mfcc_np1
+   else:
+      return labels_onehot1, x_mfcc_np1, bow_infered, lyrics_infered
 
 
 
@@ -278,10 +345,10 @@ def extract_data(audioset_path, devide_mfcc):
 def test_model(X_test, y_test, model):
    
    correct = 0
-   yhat = model.predict_classes(X_test, verbose=1)
+   yhat = model.predict_classes(X_test, verbose=0)
    #labels_np = np.array(yhat).astype(dtype=np.uint8)
    #labels_onehot = (np.arange(19) == labels_np[:, None]).astype(np.float32)
-   scores = model.evaluate(X_test, y_test, verbose=1)
+   scores = model.evaluate(X_test, y_test, verbose=0)
    for i in range(X_test.shape[0]):
       if(np.argmax(y_test[i]) == yhat[i]):
          print('Expected:', MOODS[np.argmax(y_test[i])], 'Predicted', MOODS[yhat[i]])
@@ -309,7 +376,7 @@ def train_double_LSTM(X, y, model, hidden_size):
    #model.add(Dropout(0.2))
    model.add(Dense(19, activation='softmax'))
    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])  
-   history = model.fit(X, y, epochs=100, verbose=1, batch_size=20, callbacks=[tbCallBack], shuffle=True)
+   history = model.fit(X, y, epochs=100, verbose=0, batch_size=20, callbacks=[tbCallBack], shuffle=True)
 
    # summarize history for loss
    plt.plot(history.history['loss'])
@@ -323,19 +390,38 @@ def train_double_LSTM(X, y, model, hidden_size):
    #plt.show()
    return history
 
+def audio_dnn(X, y):
+   
+   print(X.shape)
+   print(y.shape)
+   model = Sequential()
+   model.add(Dense(1024, input_shape=(X.squeeze().shape[1],), activation="sigmoid" ))
+   # now the model will take as input arrays of shape (*, 16)
+   # and output arrays of shape (*, 32)
+
+   #model.add(Activation('sigmoid'))
+   model.add(Dense(1024, activation="sigmoid"))
+   #model.add(Activation('sigmoid'))
+   model.add(Dense(y.shape[1], activation='linear'))
+
+   model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])  
+   model.fit(X.squeeze(), y, epochs=10, verbose=0, batch_size=20, callbacks=[tbCallBack], shuffle=True)
+
+
+
+   return model
 
 
 # Function to create model, required for KerasClassifier
-def create_BLSTM_model():
+def create_BLSTM_model(hidden_size, dropout, size):
    # create model
+   #hidden_size=50
+   #input_size=116
 
-   hidden_size=50
-   input_size=116
-   
    
    model = Sequential()
-   model.add(Bidirectional(LSTM(hidden_size, stateful=False), input_shape=(1,input_size)))
-   model.add(Dropout(0.5))
+   model.add(Bidirectional(LSTM(hidden_size, stateful=False), input_shape=(1, size) ))
+   model.add(Dropout(dropout))
    model.add(Dense(19, activation='softmax'))
    # Compile model
    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])  
@@ -353,7 +439,7 @@ def train_LSTM_model(X, y, model, hidden_size):
    model.add(Dropout(0.5))
    model.add(Dense(19, activation='softmax'))
    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])  
-   history = model.fit(X, y, epochs=100, verbose=1, batch_size=20, callbacks=[tbCallBack], shuffle=True)
+   history = model.fit(X, y, epochs=100, verbose=0, batch_size=20, callbacks=[tbCallBack], shuffle=True)
    """
    loss.append(history.history['loss'])
    plt.plot(history.history['loss'])
@@ -386,11 +472,12 @@ def split_data(labels, X, size):
 
 def main(argv):
    inputfile = ''
-   outputfile = ''
+   samples = -1
+   gtg = True
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,"hi:s:",["ifile=", "samples="])
    except getopt.GetoptError:
-      print( 'keras_build.py -i <inputfile> ')
+      print( 'keras_build.py -i <inputfile> -s <samples=-1> ')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
@@ -398,9 +485,10 @@ def main(argv):
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg.strip()
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg.strip()
-   start_process(inputfile)
+   
+      elif opt in ("-s","--samples"):
+         samples = int(arg.strip())
+   start_process(inputfile, samples, gtg)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
